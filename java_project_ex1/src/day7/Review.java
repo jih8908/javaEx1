55bec62599bb2c72c0c97d4a61909425b3e80656
tj-bu-706-23 <tj-bu-706-23@DESKTOP-DVU7FJ5>
2023-12-12 11:06:54 +0900

    test review

---
+++ java_project_ex1/src/day7/Review.java
@@ -0,0 +1,23 @@
+package day7;
+
+public class Review {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+//		 조건 5. 메인 메소드에서 해당 클래스를 이용하여 객체 생성 및 호출
+//         1. 객체명 : hong, 이름 : 홍길동, 학번 : 1234
+//         2. 객체명 : yu, 이름 : 유재석 : 학번 : 1357, 학과 : 컴퓨터학과
+//         3. hong 객채에서 setInfo를 통해 값 입력(나이 : 20, 성별 : 남)
+//         4. hong 객체에서 getAge 호출 후 해당 값 출력
+		
+		Student hong = new Student(1234, "홍길동동");
+		Student yu = new Student(1357, "유재석","컴퓨터공학과");
+				
+		hong.setInfo(20,"남");
+		int age = hong.getAge();
+		
+	
+	}//main
+
+}
